1e99a90ca59421eac286ffdcab2d4903ffab7af5
Wim <wim@sitebase.be>
2015-06-20 15:56:52 +0200

    make arduino network device

--- src/controller.ino
+++ src/controller.ino
@@ -1,11 +1,8 @@
-#include <SPI.h> 
+#include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-#include <Discoverable.h>
-
-Discoverable discover;
-
+int led = 13;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
 
@@ -14,16 +11,10 @@ EthernetClient ethClient;
 void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac,ip);
-  discover.begin();
 }
 
 void loop() {
 
-  if( !discover.remoteIP() ) {
-    discover.search("homeautomation");
-  } else {
-    Serial.print("Remote address is: ");
-    Serial.println(discover.remoteIP());
-  }
+  Serial.print("This is a network test");
   delay(1000);
 }
