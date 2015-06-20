ed33451cadd5cc942460968e7053c4866e677c65
Wim <wim@sitebase.be>
2015-06-20 21:13:33 +0200

    clean up the code

--- src/controller.ino
+++ src/controller.ino
@@ -56,35 +56,7 @@ void loop()
         }
     }
 
-    //endpoint();
-
-    /*if( puby ) {
-        char buffer2[40];
-        char buffer[40];
-        sprintf(buffer,"{\"action\":\"input\",\"pin\":%d}",buttonCheck);
-        sprintf(buffer2,"[%s]",buffer);
-
-        clientbla.publish("outTopic", buffer2);
-        puby = false;
-    }*/
-
     if( currentState ^ 0 ) {
-        //for(int i=1; i < 1024 ; i=i*2)  {
-            //Serial.println(i);
-            //bla += "nog";
-            /*if( currentState & i ) {
-
-                char buffer[40];
-                sprintf(buffer,"{\"action\":\"input\",\"pin\":%d},",i);
-                bla += buffer;
-
-                Serial.print("MATCH FOUND: ");
-                Serial.println(i);
-            }*/
-        //}
-
-        //Serial.println(bla);
-
         char buf[40];
         sprintf(buf, "{\"channel\":0,\"state\":%d}", (previousState ^ currentState));
         Serial.println(previousState);
@@ -114,52 +86,8 @@ void loop()
 
 }
 
-
 void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
-  Serial.println("RECEIVED mqtt data");
+    // handle message arrived
+    Serial.println("RECEIVED mqtt data");
 }
 
-/**
- * listen for an endpoint request and respond
- * with information about the current service
- */
-/*void endpoint()
-{
-    EthernetClient client = server.available();
-    if (client) {
-        // an http request ends with a blank line
-        boolean currentLineIsBlank = true;
-        while (client.connected()) {
-            if (client.available()) {
-                char c = client.read();
-                Serial.write(c);
-                // if you've gotten to the end of the line (received a newline
-                // character) and the line is blank, the http request has ended,
-                // so you can send a reply
-                if (c == '\n' && currentLineIsBlank) {
-                    client.println("HTTP/1.1 200 OK");
-                    client.println("Content-Type: application/json");
-                    client.println("Connection: close");  // the connection will be closed after completion of the response
-                    client.println();
-                    client.print("{\"name\":\"");
-                    client.print(NAME);
-                    client.print("\", \"version\": \"");
-                    client.print(VERSION);
-                    client.print("\"}");
-                    break;
-                }
-                if (c == '\n') {
-                    // you're starting a new line
-                    currentLineIsBlank = true;
-                } else if (c != '\r') {
-                    // you've gotten a character on the current line
-                    currentLineIsBlank = false;
-                }
-            }
-        }
-        // give the web browser time to receive the data
-        delay(1);
-        client.stop();
-    }
-}*/
