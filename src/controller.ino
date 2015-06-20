f08319666e6e1f6c0d548270ed850d58d79bfe36
Wim <wim@sitebase.be>
2015-06-20 21:08:30 +0200

    started to use binary to pass digital port states

--- src/controller.ino
+++ src/controller.ino
@@ -3,15 +3,15 @@
 #include <EthernetUdp.h>
 #include <PubSubClient.h>
 
-#define buttonPin 5
-#define ledPin 13
-
 char NAME[] = "home-io-controller";
 char VERSION[] = "0.0.1";
 
 int buttonState = 0;
 bool puby = false;
 int buttonCheck = 0;
+
+int previousState = 0;
+int currentState = 0;
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
@@ -27,8 +27,16 @@ void setup() {
     Ethernet.begin(mac,ip);
     server.begin();
 
-    pinMode(buttonPin, INPUT_PULLUP);
-    Button1.SetStateAndTime(LOW, 10);
+    /*pinMode(1, INPUT_PULLUP);
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);*/
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP);
+    pinMode(7, INPUT_PULLUP);
+    pinMode(8, INPUT_PULLUP);
+    /*pinMode(9, INPUT_PULLUP);
+    pinMode(10, INPUT_PULLUP);*/
 
     if (!clientbla.connected()) {
         if (clientbla.connect("arduinoClient")) {
@@ -48,22 +56,61 @@ void loop()
         }
     }
 
-    endpoint();
+    //endpoint();
 
-    if( puby ) {
+    /*if( puby ) {
+        char buffer2[40];
         char buffer[40];
         sprintf(buffer,"{\"action\":\"input\",\"pin\":%d}",buttonCheck);
-        clientbla.publish("outTopic", buffer);
+        sprintf(buffer2,"[%s]",buffer);
+
+        clientbla.publish("outTopic", buffer2);
         puby = false;
+    }*/
+
+    if( currentState ^ 0 ) {
+        //for(int i=1; i < 1024 ; i=i*2)  {
+            //Serial.println(i);
+            //bla += "nog";
+            /*if( currentState & i ) {
+
+                char buffer[40];
+                sprintf(buffer,"{\"action\":\"input\",\"pin\":%d},",i);
+                bla += buffer;
+
+                Serial.print("MATCH FOUND: ");
+                Serial.println(i);
+            }*/
+        //}
+
+        //Serial.println(bla);
+
+        char buf[3];
+        sprintf(buf, "%d", (previousState ^ currentState));
+        Serial.println(previousState);
+        Serial.println(currentState);
+        Serial.println("************");
+        clientbla.publish("outTopic", buf);
+
+        // reset
+        //previousState = currentState; // don't use this because its a super debounce that lets you only click once until you press another button
+        currentState = 0;
     }
 
+    // NOTE when I enable the two digital read lines it doesnt connect anymore to the mqtt server
+    // i think its a memory problem so maybe remove the rest endpoint from the code
 
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == LOW) {
-        // turn LED on:
-        puby = true;
-        buttonCheck = buttonPin;
-    }
+    /*if( digitalRead(1) == LOW ) currentState = currentState | 1;
+    if( digitalRead(2) == LOW ) currentState = currentState | 2;
+    if( digitalRead(3) == LOW ) currentState = currentState | 4;
+    if( digitalRead(4) == LOW ) currentState = currentState | 8;*/
+    if( digitalRead(5) == LOW ) currentState = currentState | 16;
+    if( digitalRead(6) == LOW ) currentState = currentState | 32;
+    if( digitalRead(7) == LOW ) currentState = currentState | 64;
+    if( digitalRead(8) == LOW ) currentState = currentState | 128;
+    /*if( digitalRead(9) == LOW ) currentState = currentState | 256;
+    if( digitalRead(10) == LOW ) currentState = currentState | 512;*/
+
 
 }
 
@@ -77,7 +124,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
  * listen for an endpoint request and respond
  * with information about the current service
  */
-void endpoint()
+/*void endpoint()
 {
     EthernetClient client = server.available();
     if (client) {
@@ -115,4 +162,4 @@ void endpoint()
         delay(1);
         client.stop();
     }
-}
+}*/
