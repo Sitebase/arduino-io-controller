709a5fdd9faf3ae6615985a15dc1cf0ca5243aa5
Wim <wim@sitebase.be>
2015-06-20 16:12:29 +0200

    add info endpoint

--- src/controller.ino
+++ src/controller.ino
@@ -7,14 +7,55 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
 
 EthernetClient ethClient;
+EthernetServer server(80);
 
 void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac,ip);
+  server.begin();
 }
 
 void loop() {
 
   Serial.print("This is a network test");
+
+    EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          client.println("{\"name\":\"home-io-controller\", \"version\": \"0.0.1\"}");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+}
+
   delay(1000);
+
 }
