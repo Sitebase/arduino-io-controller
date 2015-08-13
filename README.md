683974c7042569c3c8a25332ce43b9226f23c41f
Wim <wim@sitebase.be>
2015-08-13 08:13:13 +0200

    clean up code

--- README.md
+++ README.md
@@ -5,40 +5,17 @@ This project is a prototype that should eventually replace the PLC that I'm now 
 The only thing this component should do is controlling IO, meaning dispatching an event on the network when an input is received, and setting the states of outputs.
 This functionality can then be made available over a API that is available on the network.
 
-By keeping the controller so simple it's making it possible to write all the logic, of what switch should control what light, in a language/server/OS your prefer.
+By keeping the controller so simple it's making it possible to write all the logic, of what switch should control what light, in a language/server/OS you prefer.
 
 ## Reason
 
-* For the moment my house is running on a Beckhoff CX9000 PLC. Apart from the fact that it's running Window CE you are locked into a horrible old IDE that only runs well on
+* For the moment my house is running on a Beckhoff CX9000 PLC. Apart from the fact that it's running Window CE you are locked into a horrible old IDE that only runs "well" on
 a Window XP system.
 * I like to build my own stuff :)
 
-## Develop
-
-    node server.js | bunyan
-    ./build
-    /usr/local/Cellar/mosquitto/1.3.5/sbin/mosquitto -p 1883 -v
-    node client.js
-
-## Messages
-
-* val (`digital read`): Values of the digital inputs configured as inputs
-* c (`channel`): Each IO controller has it's own channel so that you know from what controller you receive the input. While sending a command this can also be used to target a specific IO controller.
-* val[0-5] (`analog read`): analog readed values
-* cmd (`command`): execute a command. e.g. dr, ar, ds
-
-a `dr` command is both triggered by an input change or by sending the `dr` command. The latter case can be used to retrieve the current state of the digital input pins.
-
-|  Topic  |                Message                 |                 Description                  |
-| ------- | -------------------------------------- | -------------------------------------------- |
-| state   | {"c":0,"cmd":"dr","val":8}             | State of the digital inputs                  |
-| state   | {"c":0,"cmd":"ar","val0":200,"val0":2} | Values of the analoge inputs                 |
-| command | {"c":0,"cmd":"dr"}                     | Command to request digital input values      |
-| command | {"c":0,"cmd":"ar"}                     | Command to request analoge input values      |
-| command | {"c":0,"cmd":"ds","v":1,"p":13}        | Command to set the digital output 13 to high |
-
 ## Goals
 
+* First goal: create a fixed example that uses an input and toggles an output, this simple implementation is just a test to see how reliable this runs 24/7
 * Listen for events on inputs, for this first prototype I will be using polling because the Arduino UNO I'm using has only a few interrupts
 * Distribute events over the network using MQTT
 * Set output states using MQTT
@@ -48,6 +25,12 @@ a `dr` command is both triggered by an input change or by sending the `dr` comma
 
 * [Button click types detect](http://forum.arduino.cc/index.php?topic=14479.0)
 
-## Todo
+## Raspberry Pi
 
-* Publish from node client is not received by the arduino, while mosquitto does receive it and sends it off to the arduinoClient
+Configure to send all syslog messages to main server. To do this edit `/etc/rsyslog.conf`
+
+    *.* 192.168.1.117:514
+
+ Restart service after edit:
+
+    sudo service rsyslog restart
--- src/controller.ino
+++ src/controller.ino
@@ -1,204 +1,48 @@
 #include <SPI.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
-#include <PubSubClient.h>
-#include <DHT.h>
 #include <String.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
 
-#define channel 0
-
-int previousState = 0;
-int currentState = 0;
-
-int counter = 0;
-#define DHTPIN 2
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
-
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 177);
-byte remote[] = { 192, 168, 1, 117 };
-
-EthernetClient ethClient;
-EthernetServer server(80);
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte server[] = { 192, 168, 1, 117 };
+byte ip[]     = { 192, 168, 1, 188 };
 
 void callback(char* topic, byte* payload, unsigned int length) {
-
-    Serial.print("callback fired: ");
-    Serial.print(topic);
-    Serial.print(" - ");
-    Serial.print((char)payload[0]);
-    Serial.print(",");
-    Serial.print(payload[1]);
-    Serial.print(",");
-    Serial.print(payload[2]);
-    Serial.print(",");
-    Serial.print(payload[3]);
-    Serial.print(",");
-    Serial.print(payload[4]);
-    Serial.println(";");
-
-    if( (char)payload[0] == '{' )
-    {
-        Serial.println("JSON detected");
-    }
+  // handle message arrived
 }
 
-PubSubClient clientbla(remote, 1883, callback, ethClient);
+EthernetClient ethClient;
+PubSubClient client(server, 1883, callback, ethClient);
 
 void setup() {
+
+    Ethernet.begin(mac, ip);
+    if (client.connect("arduinoClient")) {
+        client.publish("outTopic","hello world");
+        client.subscribe("inTopic");
+    }
+
     Serial.begin(9600);
-    Ethernet.begin(mac,ip);
-    server.begin();
-
-    //pinMode(1, INPUT_PULLUP);
-    pinMode(2, INPUT_PULLUP);
-    pinMode(3, INPUT_PULLUP);
-    //pinMode(4, INPUT_PULLUP);
-    pinMode(5, INPUT_PULLUP);
-    pinMode(6, INPUT_PULLUP);
-    pinMode(7, INPUT_PULLUP);
-    pinMode(8, INPUT_PULLUP);
-    pinMode(9, INPUT_PULLUP);
-    //pinMode(10, INPUT_PULLUP);
-
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-
-    digitalWrite(12, HIGH);
-
-    /*if (!clientbla.connected()) {
-        if (clientbla.connect("arduinoClient")) {
-            Serial.println("Connected to server");
-            clientbla.subscribe("command");
-        }
-    }*/
-
+    //pinMode(11, OUTPUT);
+    pinMode(12, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
+    Serial.println("waiting for button press ...");
 }
 
 void loop()
 {
 
-    // auto reconnect
-    /*Serial.println("before connect");
-    if (!clientbla.connected()) {
-        Serial.println("not connected");
-        if (clientbla.connect("arduinoClient")) {
-            Serial.println("Connected to server");
-            clientbla.subscribe("command");
-        }
-    }*/
+    client.loop();
 
-    if( currentState ^ 0 ) {
+    int buttonState1 = digitalRead(12);
+    int buttonState2 = digitalRead(13);
 
-        // calculate buffer length needed for the JSON message
-        int state = (previousState ^ currentState);
-        char* format = "{\"c\":%d,\"cmd\":\"dr\",\"val\":%d}";
-        int length = strlen(format) + sizeof(channel) + sizeof(state);
-        char buf[length];
+    if( buttonState1 == HIGH )
+        Serial.println("Button 1 pressed");
 
-        // construct JSON message
-        sprintf(buf, format, channel, state);
+    if( buttonState2 == HIGH )
+        Serial.println("Button 2 pressed");
 
-        // publish message
-        clientbla.publish("state", buf);
-
-        // reset
-        currentState = 0;
-    }
-
-    // NOTE when I enable the two digital read lines it doesnt connect anymore to the mqtt server
-    // i think its a memory problem so maybe remove the rest endpoint from the code
-
-    //if( digitalRead(1) == LOW ) currentState = currentState | 1; // is always low
-    if( digitalRead(2) == LOW ) currentState = currentState | 2;
-    if( digitalRead(3) == LOW ) currentState = currentState | 4;
-    //if( digitalRead(4) == LOW ) currentState = currentState | 8; // this input is broken on my UNO, it's always low
-    if( digitalRead(5) == LOW ) currentState = currentState | 16;
-    if( digitalRead(6) == LOW ) currentState = currentState | 32;
-    if( digitalRead(7) == LOW ) currentState = currentState | 64;
-    if( digitalRead(8) == LOW ) currentState = currentState | 128;
-    if( digitalRead(9) == LOW ) currentState = currentState | 256;
-    //if( digitalRead(10) == LOW ) currentState = currentState | 512; // doesn't connect to mqtt anymore if you enable this
-
-    /*counter++;
-    if( counter > 20000)
-    {
-        sensors();
-
-        int analog0 = analogRead(A0);
-        int analog1 = analogRead(A1);
-        int analog2 = analogRead(A2);
-        int analog3 = analogRead(A3);
-        int analog4 = analogRead(A4);
-        int analog5 = analogRead(A5);
-
-
-        // Reading temperature or humidity takes about 250 milliseconds!
-          // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-          float h = dht.readHumidity();
-          // Read temperature as Celsius
-          float t = dht.readTemperature();
-          Serial.print("temp: ");
-          Serial.println(t);
-          Serial.print("hum: ");
-          Serial.println(h);
-
-          //Serial.println(getDecimal(t));
-          int tester = int(t);
-          String value = String(tester);
-          Serial.println(tester);
-          Serial.println(value);
-
-        char* output = "{\"c\":%d,\"cmd\":\"ar\",\"val0\":%d,\"val1\":%d,\"val2\":%d,\"val3\":%d,\"val4\":%d,\"val5\":%d}";
-        int olength = strlen(output) + sizeof(channel) + sizeof(analog0) + sizeof(analog1) + sizeof(analog2) + sizeof(analog3) + sizeof(analog4) + sizeof(analog5);
-        char buf2[olength];
-
-        // construct JSON message
-        sprintf(buf2, output, channel, analog0, analog1, analog2, analog3, analog4, analog5);
-
-        clientbla.publish("state", buf2);
-        counter = 0;
-    }
-
-    clientbla.loop();*/
-
-}
-
-void sensors()
-{
-    float h = dht.readHumidity();
-    // Read temperature as Celsius
-    float t = dht.readTemperature();
-    Serial.print("temp: ");
-    Serial.println(t);
-    Serial.print("hum: ");
-    Serial.println(h);
-
-    //Serial.println(getDecimal(t));
-    int temp = int(t);
-    int hum = int(h);
-
-    char* output = "{\"c\":%d,\"cmd\":\"sensor\",\"temperature\":%d,\"humidity\":%d}";
-    int olength = strlen(output) + sizeof(channel) + sizeof(temp) + sizeof(hum);
-    char buf2[olength];
-
-    // construct JSON message
-    sprintf(buf2, output, channel, temp, hum);
-
-    clientbla.publish("/state/sensor", buf2);
-}
-
-
-//function to extract decimal part of float
-long getDecimal(float val)
-{
- int intPart = int(val);
- long decPart = 1000*(val-intPart); //I am multiplying by 1000 assuming that the foat values will have a maximum of 3 decimal places
-                                   //Change to match the number of decimal places you need
- if(decPart>0)return(decPart);           //return the decimal part of float number if it is available
- else if(decPart<0)return((-1)*decPart); //if negative, multiply by -1
- else if(decPart=0)return(00);           //return 0 if decimal part of float number is not available
+    delay(50);
 }
